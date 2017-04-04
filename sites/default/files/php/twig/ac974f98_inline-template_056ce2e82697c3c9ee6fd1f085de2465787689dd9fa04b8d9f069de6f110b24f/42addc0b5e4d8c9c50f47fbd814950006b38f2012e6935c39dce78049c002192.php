ae1f4e55552bd7d35951c5149746f6fc280c8c32
Rajesh Saini <towards2sky@gmail.com>
2017-04-04 19:12:49 +0530

    add files dir

---
+++ sites/default/files/php/twig/ac974f98_inline-template_056ce2e82697c3c9ee6fd1f085de2465787689dd9fa04b8d9f069de6f110b24f/42addc0b5e4d8c9c50f47fbd814950006b38f2012e6935c39dce78049c002192.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {# inline_template_start #}node/{{ nid }} */
+class __TwigTemplate_8594243bea42a3f79fb45ae80f56c5b75db702bd72736d528fbabd9f95986a3f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "node/";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["nid"]) ? $context["nid"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}node/{{ nid }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}node/{{ nid }}*/
---
+++ sites/default/files/php/twig/ac974f98_inline-template_94ad35ce71dcd75574dda03c435813c7571d240462890ff58235dd5fe0c4eba1/9ef830212e0a284d1981366f18f0773f7083919cd8ef35a83f7235f7427e15f7.php
@@ -0,0 +1,64 @@
+<?php
+
+/* {# inline_template_start #}{{ title__value }}, {{ field_unitversity_located }}  */
+class __TwigTemplate_03360ac0ca220cdb0f8c75cdfc255f1f8a3c1a25f75a588918cd25aa035ba145 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title__value"]) ? $context["title__value"] : null), "html", null, true));
+        echo ", ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["field_unitversity_located"]) ? $context["field_unitversity_located"] : null), "html", null, true));
+        echo " ";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}{{ title__value }}, {{ field_unitversity_located }} ";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}{{ title__value }}, {{ field_unitversity_located }} */
